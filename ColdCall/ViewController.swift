cfccaa3d8671d386fc101b4cfb25a0f869291de8
Zahra Al-Jafar <memo.jks357@hotmail.com>
2021-12-09 00:27:40 +0000

    Cold Call Version #1

--- ColdCall/ViewController.swift
+++ ColdCall/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var namesLabel: UILabel!
+    
+    let namesArray: [String] = ["Jimmy", "Cody", "Bryant", "Royta", "Jay"]
+
+    
+    @IBAction func coldCallPressed(_ sender: UIButton) {
+        let position = Int.random(in: 0..<5)
+        namesLabel.text = "\(namesArray[position])"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        namesLabel.text = "Ready?"
     }
 
 
